de9b508b02e92f23db76eaa9258c6e01841875f5
mathis <mathismartini1@gmail.com>
2021-11-06 22:47:33 +0100

    Update add utils

--- get_next_line.h
+++ get_next_line.h
@@ -5,16 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/06 16:49:25 by mathmart          #+#    #+#             */
-/*   Updated: 2021/11/06 16:51:09 by mathmart         ###   ########.fr       */
+/*   Created: 2021/11/04 14:33:54 by mathmart          #+#    #+#             */
+/*   Updated: 2021/11/06 19:08:53 by mathmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
-# include <unistd.h>
 
-# endif
+# define BUFFER_SIZE 12
+
+short	ft_get_nl(char *bakup);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strcpy(char *dest, const char *str);
+size_t	ft_strlen(const char *str);
+
+#endif
